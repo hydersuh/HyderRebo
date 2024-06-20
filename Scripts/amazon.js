2d873f9119af6acfd6b835dddc6e7bc892b20903
hydersoft@yahoo.com <hydersoft@yahoo.com>
2024-06-20 05:29:18 -0700

    Added new js File conain all products

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -1,28 +1,3 @@
-const products = [{
-    Image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Gotton Socks - 6pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    Image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-}, {
-    Image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: ' Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
 
 let productsHTML = '';
 
@@ -31,7 +6,7 @@ products.forEach((product) => {
     <div class="products-container">
             <div class="products-image-container">
                 <img class="products-image" 
-                src="${product.Image}">
+                src="${product.image}">
             </div>
             <div class="product-name item-text-to-2-lines">
                 ${product.name}
