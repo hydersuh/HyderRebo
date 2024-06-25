4b247a8c990cab096f779aa831aa2a2477a9ac7f
hydersoft@yahoo.com <hydersoft@yahoo.com>
2024-06-25 14:09:28 -0700

    Create oop version of cart

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,5 +1,6 @@
 import {renderOrderSummart} from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';
 
 renderOrderSummart();
 renderPaymentSummary();
