ddc59c22f68095eddb4874bf530d4be6b9116e50
hydersoft@yahoo.com <hydersoft@yahoo.com>
2024-06-26 14:15:36 -0700

    Add backend practice file

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummart} from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
+import '../data/backend-practice.js'
 
 renderOrderSummart();
 renderPaymentSummary();
