3059f17bd3a015081ae4e01e9c0eebf11a3d1a2c
hydersoft@yahoo.com <hydersoft@yahoo.com>
2024-06-26 16:39:26 -0700

    Learn promisis and covert code to promises

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,11 +1,54 @@
 import {renderOrderSummart} from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js'
 
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1');
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
 
-loadProducts(() => {
+    ]).then((values) => {
+        console.log(values);
+        renderOrderSummart();
+        renderPaymentSummary();
+    });
+
+/*
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');
+    });
+
+}).then((value) => {
+    console.log(value);
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    }); 
+
+}).then(() => {
     renderOrderSummart();
     renderPaymentSummary();
 });
+*/
+
+
+/*
+loadProducts(() => { 
+    loadCart(() => {
+        renderOrderSummart();
+        renderPaymentSummary(); 
+    });  
+});
+*/
