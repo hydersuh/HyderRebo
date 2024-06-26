9bcc26da6f36620929c1516a201782d5653722b5
hydersoft@yahoo.com <hydersoft@yahoo.com>
2024-06-25 17:14:57 -0700

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from '../Scripts/utils/money.js';
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -7,7 +9,32 @@ export function getProduct(productId) {
       }
   });
 
-  return matchingProduct;
+  return matchingProduct; 
+
+}
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+    getStarsUrl() {
+      return `images/ratings/rating-${this.rating.stars * 10}.png`;
+    }
+
+    getPrice() {
+     return `$${formatCurrency(this.priceCents)}`
+    }
 }
 
 
@@ -670,4 +697,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails);
+});
