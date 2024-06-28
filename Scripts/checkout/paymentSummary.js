329a4e4e1e6f54e3eb918e1b884a172a909fea7e
hydersoft@yahoo.com <hydersoft@yahoo.com>
2024-06-27 20:49:08 -0700

    Create order using the backend

--- Scripts/checkout/paymentSummary.js
+++ Scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOption.js';
 import {formatCurrency} from '../utils/money.js';
+import { addOrder } from '../../data/orders.js';
 
 
 export function renderPaymentSummary() {
@@ -61,12 +62,36 @@ export function renderPaymentSummary() {
     </div>
     </div>
 
-    <button class="place-order-button button-primary">
+    <button class="place-order-button button-primary
+    js-place-order">
         place your order
 </button>
     `;
 
     document.querySelector('.js-payment-summary')
         .innerHTML = paymentSummaryHTML;
+
+    document.querySelector('.js-place-order')
+        .addEventListener('click', async () => {
+            try{
+                const response = await fetch('https://supersimplebackend.dev/orders', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify({
+                cart: cart
+            })
+           }); 
+
+           const order = await response.json();
+           addOrder(order);
+            } catch(error) {
+                console.log('unexpected error. try again later')
+
+            }
+           
+            window.location.href = '03-Third-Project.html';
+        });
 
 }
